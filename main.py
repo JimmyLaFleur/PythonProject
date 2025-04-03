0971d332ed5089ce6f9d076b3857d305c2658ea3
knyazhe <olegkzn0@gmail.com>
2025-04-03 04:18:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,6 @@ from telebot import types
 import DBManager
 import config
 
-'''
-bot = telebot.TeleBot(token='7469709374:AAHtJR9Jsw359d_7TuMGSsT2AFzJrd7CtI0')
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.from_user.id, "привет пришли мне файл и я распечатаю его")
-@bot.message_handler(content_types=['document'])
-def handle_docs_photo(message):
-    try:
-        chat_id = message.chat.id
-        printer.delete_files_in_folder('files')
-        file_info = bot.get_file(message.document.file_id)
-        print(file_info)
-        downloaded_file = bot.download_file(file_info.file_path)
-        print(downloaded_file)
-        src = 'files/' + message.document.file_name
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
-
-        bot.reply_to(message, "Пожалуй, я распечатаю это")
-        print(src)
-        printer.print_file(src)
-        bot.reply_to(message, "Распечатал")
-    except Exception as e:
-        bot.reply_to(message, e)
-
-'''
 
 bot = telebot.TeleBot(config.TOKEN)
 connection = DBManager.connect()
