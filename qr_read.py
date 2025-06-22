320d7d1034c8e34caa38d5a3409db010328c536f
Vladislav Savchenko <osnovavlad2005@gmail.com>
2025-06-22 10:39:37 +0300

    Delete qr_read.py

--- qr_read.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from pyzbar import pyzbar
-
-def qr_to_text(qr_image):
-    img = cv2.imread(qr_image)
-    barcodes = pyzbar.decode(img)
-    for barcode in barcodes:
-        barcodeData = barcode.data.decode("utf-8")
-        print(barcodeData)
