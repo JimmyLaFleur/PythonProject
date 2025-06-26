0ad8fd4735fd233f127cfbb28814f611590e32dd
Vladislav Savchenko <osnovavlad2005@gmail.com>
2025-06-26 09:01:37 +0300

    Update DBManager.py
    Расширение БД, добавление новых tables

--- DBManager.py
+++ DBManager.py
@@ -6,6 +6,8 @@ def connect():
 def drop_tables(connection):
     with connection:
         connection.executescript('''
+        DROP TABLE IF EXISTS event_participants;
+        DROP TABLE IF EXISTS events;
         DROP TABLE IF EXISTS spendings;
         DROP TABLE IF EXISTS users;
         ''')
@@ -15,7 +17,7 @@ def create_tables(connection):
         connection.executescript('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            telegram_id INTEGER UNIQUE
+            telegram_id TEXT UNIQUE
         );
 
         CREATE TABLE IF NOT EXISTS spendings (
@@ -24,6 +26,19 @@ def create_tables(connection):
             price INTEGER NOT NULL,
             title TEXT NOT NULL,
             date TEXT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS event_participants (
+            event_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            PRIMARY KEY (event_id, user_id),
+            FOREIGN KEY (event_id) REFERENCES events(id),
             FOREIGN KEY (user_id) REFERENCES users(id)
         );
         ''')
